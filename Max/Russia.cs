0d9c3a8c4eb2a3f3198a990ea659945e91d1709d
Max Kersten <max-kersten@live.nl>
2018-09-03 21:31:59 +0200

    I added my folder!

---
+++ Max/Russia.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Maskirovka
+{
+
+	public class Russia : MonoBehaviour 
+	{
+		public List<Neighbour> neighbours;
+		private Color color;
+				
+		void Start () 
+		{
+			color = GetComponent<SpriteRenderer>().color;
+		}
+
+		public bool InvadeCountry( Country country )
+		{
+			bool isNeighbour = neighbours.FindAll( x => x.neighbour ==  country ).Count > 0;
+			
+			if( isNeighbour == true )
+			{
+				country.Invaded(this);
+			}
+			return isNeighbour;
+		}
+
+		public Color GetColor()
+		{
+			return color;
+		}
+
+		public bool IsNeighbour(Country country)
+		{
+			return neighbours.FindAll( x => x.neighbour == country).Count > 0;
+		}
+
+		public void AddNeighbours( Neighbour[] newNeighbours )
+		{
+			foreach( Neighbour n in newNeighbours)
+			{
+				if( neighbours.FindIndex( x => x.neighbour == n.neighbour ) == -1 )
+				{
+					neighbours.Add( n );
+				}
+			}
+		}
+	}
+}
