0758676261f7f012f33b2cfea593fdc7fffba835
Bob <bobjeltes@hotmail.com>
2018-09-04 10:53:39 +0200

    Klaar voor review
    Gefixt, als het goed is naar behoren volgens de afgesproken conventies. Kraak vooral af.

--- ClockScript.cs
+++ ClockScript.cs
@@ -12,22 +12,28 @@ public class ClockScript : MonoBehaviour {
     public GameObject timesUpScreen;
     public GameObject eggSpawner;
 
+    //Winner screens
     public GameObject winnerScreen_P1;
     public GameObject winnerScreen_P2;
     public GameObject drawScreen;
 
+    //Win texts
     public Text pointsTextWin_P1;
     public Text pointsTextWin_P2;
 
+    //Lose texts
     public Text pointsTextLose_P1;
     public Text pointsTextLose_P2;
 
+    //Draw texts
     public Text pointsTextDraw_P1;
     public Text pointsTextDraw_P2;
 
+    //Score displays
     public GameObject scoreDisplay_P1;
     public GameObject scoreDisplay_P2;
 
+    //Time variables
     int startTime = 30;
     int time;
 
@@ -40,23 +46,19 @@ public class ClockScript : MonoBehaviour {
 
     bool canContinue = false;
 
-    // Use this for initialization
-    void Start () {
+    void Start(){
         time = startTime;
         StartCoroutine("CountdownTime");
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	void Update(){}
         clockText.text = time.ToString();
 
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
+        if (Input.GetKeyDown(KeyCode.Escape)){
             ReturnToMainMenu();
         }
 
-        if (time == 0)
-        {
+        if (time == 0){
             // Stop countdown timer and activate TIME'S UP screen
             StopCoroutine("CountdownTime");
             timesUpScreen.SetActive(true);
@@ -73,8 +75,7 @@ public class ClockScript : MonoBehaviour {
 	}
 
     // Do I really need to explain this tho
-    IEnumerator CountdownTime ()
-    {
+    IEnumerator CountdownTime (){
         while (time > 0)
         {
             yield return new WaitForSeconds(1);
@@ -84,8 +85,7 @@ public class ClockScript : MonoBehaviour {
     }
 
     // Post-game screen coroutine
-    IEnumerator PostGame ()
-    {
+    IEnumerator PostGame (){
         // Wait 2 seconds before showing the results screen
         yield return new WaitForSeconds(2);
 
@@ -99,20 +99,17 @@ public class ClockScript : MonoBehaviour {
         //winnerScreen_P2Text.text = scoreText_P2.ToString();
 
         // Display winner's screen
-        if (score.score_P1 > score.score_P2)
-        {
+        if (score.score_P1 > score.score_P2){
             pointsTextWin_P1.text = scoreString_P1;
             pointsTextLose_P2.text = scoreString_P2;
             winnerScreen_P1.SetActive(true);
         }
-        else if (score.score_P1 < score.score_P2)
-        {
+        else if (score.score_P1 < score.score_P2){
             pointsTextWin_P2.text = scoreString_P2;
             pointsTextLose_P1.text = scoreString_P1;
             winnerScreen_P2.SetActive(true);
         }
-        else if (score.score_P1 == score.score_P2)
-        {
+        else if (score.score_P1 == score.score_P2){
             pointsTextDraw_P1.text = scoreString_P1;
             pointsTextDraw_P2.text = scoreString_P2;
             drawScreen.SetActive(true);
@@ -120,22 +117,18 @@ public class ClockScript : MonoBehaviour {
 
         yield return new WaitForSeconds(1);
         canContinue = true;
-        if (canContinue)
-        {
+        if (canContinue){
             postGameMenu.SetActive(true);
-            if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
-            {
+            if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter)){
                 SceneManager.LoadScene(SceneManager.GetActiveScene().name);
             }
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
+            if (Input.GetKeyDown(KeyCode.Space)){
                 ReturnToMainMenu();
             }
         }
     }
 
-    void ReturnToMainMenu()
-    {
+    void ReturnToMainMenu(){
         Destroy(musicPlayerGame);
         SceneManager.LoadScene(sceneBuildIndex: 1);
     }
