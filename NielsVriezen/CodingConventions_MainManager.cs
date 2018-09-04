ad0df2ad1ba373a072d366275dd15f03a360586f
NVriezen <36536400+NVriezen@users.noreply.github.com>
2018-09-04 10:32:01 +0200

    D:\HKU\Jaar2\KernModuleDev\Blok1\NielsVriezen\NielsVriezen

--- NielsVriezen/CodingConventions_MainManager.cs
+++ NielsVriezen/CodingConventions_MainManager.cs
@@ -6,31 +6,37 @@ using UnityEngine.SceneManagement;
 
 public class MainManager : MonoBehaviour {
 
-	MainManager instance;
+	private MainManager instance;
 
-	string[ , ] userTimeArray;
-	int currentPlayer = 0;
-	float userTime;
-	string userName;
-	public float startUserTime;
-	public float EndUserTime;
+	public float startUserTime {
+		get;
+		protected set;
+	}
+	public float endUserTime {
+		get;
+		protected set;
+	}
+
+	private string[ , ] userTimeArray;
+	private int currentPlayer = 0;
+	private float userTime;
+	private string userName;
+
 
 	// Use this for initialization
-	void Awake () {
-		if (Display.displays.Length > 1)
-			Display.displays[1].Activate();
-		if (Display.displays.Length > 2)
-			Display.displays[2].Activate();
-		if (this != instance && instance == null) {
-			instance = this;
-		} else {
+	private void Awake () {
+		if (Display.displays.Length > 1) Display.displays[1].Activate();
+		if (Display.displays.Length > 2) Display.displays[2].Activate();
+		if (this != instance && instance == null) instance = this;
+		else {
 			Destroy (this.gameObject);
 		}
 		DontDestroyOnLoad (instance.gameObject);
 	}
-	
+
+
 	// Update is called once per frame
-	void Update () {
+	private void Update () {
 		if (Input.GetMouseButtonDown (1)) {
 			userTime = 899;
 			SaveScore ();
@@ -38,27 +44,26 @@ public class MainManager : MonoBehaviour {
 		
 	}
 
+
 	public void OnEndEdit(){
 		userName = GameObject.Find("InputField").GetComponent<InputField> ().text;
 		GameObject.Find ("confirmationText").GetComponent<Animator> ().Play ("ConfirmationAnimation");
 		Debug.Log (userName);
-		//userTimeArray[0,0] = {{userName, userTime},{"0",""}};
-		//++currentPlayer;
 	}
+
 
 	public void OnLogin(){
 		OnEndEdit ();
 		SceneManager.LoadSceneAsync (1);
 	}
 
+
 	public void SaveScore(){
-		//convert time;
 		userTime = (EndUserTime - startUserTime);
 
 		float[] oldTime = PlayerPrefsX.GetFloatArray("HighscoresTimes", 900, 100);
 		string[] oldName = PlayerPrefsX.GetStringArray("HighscoresNames", "AI 1.2", 100);
-		//int[] highscores = new int[oldHigh.Length];
-		//int oldH = 0;
+
 		for (int i = 0; i < oldTime.Length; i++){
 			float oldT = oldTime[i];
 			string oldN = oldName [i];
@@ -69,21 +74,10 @@ public class MainManager : MonoBehaviour {
 				userName = oldN;
 			}
 		}
+
 		PlayerPrefsX.SetFloatArray ("HighscoresTimes", oldTime);
 		PlayerPrefsX.SetStringArray ("HighscoresNames", oldName);
-		//string[] oldName = PlayerPrefsX.GetStringArray("HighscoresNames", 0, 100);
-		//int[] highscores = new int[oldHigh.Length];
-		//int oldH = 0;
-		/*for (int i = 0; i < oldName.Length; i++){
-			string oldN = oldName[i];
-			if (userName > oldName[i]) {
-				oldName[i] = userTime;
-				userName = oldN;
-			}
-		}
-		*/
-		//PlayerPrefsX.SetFloatArray ("HighscoresNames", oldName);
-
-		//Debug.Log (oldName[1] + " -- " + oldTime[1]);
 	}
+
+
 }
