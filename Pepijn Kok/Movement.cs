2f8d520d7773b13b1325aa3fd0b7eb553fcdf254
Atlas <AtlasTheTitan77@gmail.com>
2018-09-04 10:54:13 +0200

    i added my folder
    Ik hoop dat het goed is en dat ik niks ben vergeten

---
+++ Pepijn Kok/Movement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+	public float speed {
+		get; set = 1f;
+	}
+	public float camSpeed {
+		get; set = 1.0f;
+	}
+	private float z = 0;
+	private float x = 0;
+	private float xCam = 0;
+	private Vector3 EulerAngleVelocity;
+
+
+	void Update () {
+
+		z = Input.GetAxis("Vertical") * speed;
+		x = Input.GetAxis("Horizontal") * speed;
+		xCam = Input.GetAxis("CameraHorizontal") * camSpeed;
+
+		GetComponent<Rigidbody>().AddForce(this.transform.forward * z);
+		GetComponent<Rigidbody>().AddForce(this.transform.right * x);
+
+		EulerAngleVelocity = new Vector3(0, xCam, 0);
+		Quaternion deltaRotation = Quaternion.Euler(EulerAngleVelocity);
+        GetComponent<Rigidbody>().MoveRotation(GetComponent<Rigidbody>().rotation * deltaRotation);
+	}
+}
