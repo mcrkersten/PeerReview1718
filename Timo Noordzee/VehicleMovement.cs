3317187ede4a22a8e94d017c020b7a331804845e
Timo Noordzee <timo_noordzee@hotmail.com>
2018-09-05 21:47:07 +0200

    Reformatted code
    Reformatted code according to code conventions

--- Timo Noordzee/VehicleMovement.cs
+++ Timo Noordzee/VehicleMovement.cs
@@ -4,35 +4,118 @@ public class VehicleMovement : MonoBehaviour {
     public float speed;
 
     [Header("Drive Settings")]
-    public float driveForce = 17f; //The force that the engine generates
-    public float slowingVelFactor = .99f; //The percentage of velocity the ship maintains when not thrusting (e.g., a value of .99 means the ship loses 1% velocity when not thrusting)
-    public float brakingVelFactor = .95f; //The percentage of velocty the ship maintains when braking
-    public float angleOfRoll = 30f; //The angle that the ship "banks" into a turn
-    public float angleOfWheelsRoll = 60f;
-    public float turningSpeed = 10.0f;
+    //The force that the engine generates
+    [SerializeField] private float _driveForce = 17f;
+    public float driveForce {
+        get { return _driveForce; }
+        set { _driveForce = value; }
+    }
+    //The percentage of velocity the ship maintains when not thrusting (e.g., a value of .99 means the ship loses 1% velocity when not thrusting)
+    [SerializeField] private float _slowingVelFactor = .99f;
+    public float slowingVelFactor {
+        get { return _slowingVelFactor; }
+        set { _slowingVelFactor = value; }
+    }
+    //The percentage of velocty the ship maintains when braking
+    [SerializeField] private float _brakingVelFactor = .95f;
+    public float brakingVelFactor {
+        get { return _brakingVelFactor; }
+        set { _brakingVelFactor = value; }
+    }
+    //The angle that the ship "banks" into a turn
+    [SerializeField] private float _angleOfRoll = 30f;
+    public float angleOfRoll {
+        get { return _angleOfRoll; }
+        set { _angleOfRoll = value; }
+    }
+    [SerializeField] private float _angleOfWheelsRoll = 60f;
+    public float angleOfWheelsRoll {
+        get { return _angleOfWheelsRoll; }
+        set { _angleOfRoll = value; }
+    }
+    [SerializeField] private float _turningSpeed = 10.0f;
+    public float turningSpeed {
+        get { return _turningSpeed; }
+        set { _turningSpeed = value; }
+    }
+    public float speed { get; set; }
 
     [Header("Hover Settings")]
-    public float hoverHeight = 1.5f; //The height the ship maintains when hovering
-    public float maxGroundDist = 5f; //The distance the ship can be above the ground before it is "falling"
-    public float hoverForce = 300f; //The force of the ship's hovering
-    public LayerMask whatIsGround; //A layer mask to determine what layer the ground is on
-    public PIDController hoverPID; //A PID controller to smooth the ship's hovering
+    //The height the ship maintains when hovering
+    [SerializeField] private float _hoverHeight = 1.5f;
+    public float hoverHeight {
+        get { return _hoverHeight; }
+        set { _hoverHeight = value; }
+    }
+    //The distance the ship can be above the ground before it is "falling"
+    [SerializeField] private float _maxGroundDist = 5f;
+    public float maxGroundDist {
+        get { return _maxGroundDist; }
+        set { _maxGroundDist = value; }
+    }
+    //The force of the ship's hovering
+    [SerializeField] private float _hoverForce = 300f;
+    public float hoverForce {
+        get { return _hoverForce; }
+        set { _hoverForce = value; }
+    }
+    //A layer mask to determine what layer the ground is on
+    [SerializeField] private LayerMask _whatIsGround;
+    public LayerMask whatIsGround {
+        get { return _whatIsGround; }
+        set { _whatIsGround = value; }
+    }
+    //A PID controller to smooth the ship's hovering
+    [SerializeField] private PIDController _hoverPID;
+    public PIDController hoverPID {
+        get { return _hoverPID; }
+        set { _hoverPID = value; }
+    }
 
     [Header("Physics Settings")]
-    public Transform shipBody; //A reference to the ship's body, this is for cosmetics
-    public Transform wheelsFront;
-    public Transform wheelsBack;
-    public float terminalVelocity = 100f; //The max speed the ship can go
-    public float hoverGravity = 20f; //The gravity applied to the ship while it is on the ground
-    public float fallGravity = 80f; //The gravity applied to the ship while it is falling
+    //A reference to the ship's body, this is for cosmetics
+    [SerializeField] private Transform _shipBody;
+    public Transform shipBody {
+        get { return _shipBody; }
+        set { _shipBody = value; }
+    }
+    [SerializeField] private Transform _wheelsFront;
+    public Transform wheelsFront {
+        get { return _wheelsFront; }
+        set { _wheelsFront = value; }
+    }
+    [SerializeField] private Transform _wheelsBack;
+    public Transform wheelsBack {
+        get { return _wheelsBack; }
+        set { _wheelsBack = value; }
+    }
+    //The max speed the ship can go
+    [SerializeField] private float _terminalVelocity = 100f;
+    public float terminalVelocity {
+        get { return _terminalVelocity; }
+        set { _terminalVelocity = value; }
+    }
+    //The gravity applied to the ship while it is on the ground
+    [SerializeField] private float _hoverGravity = 20f;
+    public float hoverGravity {
+        get { return _hoverGravity; }
+        set { hoverGravity = value; }
+    }
+    //The gravity applied to the ship while it is falling
+    [SerializeField] private float _fallGravity = 80f;
+    public float fallGravity {
+        get { return _fallGravity; }
+        set { _fallGravity = value; }
+    }
 
-    Rigidbody rigidBody; //A reference to the ship's rigidbody
-    PlayerInput input; //A reference to the player's input					
-    float drag; //The air resistance the ship recieves in the forward direction
-    bool isOnGround; //A flag determining if the ship is currently on the ground
+    private Rigidbody rigidBody; //A reference to the ship's rigidbody
+    private PlayerInput input; //A reference to the player's input					
+    private float drag; //The air resistance the ship recieves in the forward direction
+    private bool isOnGround; //A flag determining if the ship is currently on the ground
     private AudioSource audioSource;
 
-    void Start() {
+
+    private void Start() {
         //Get references to the Rigidbody and PlayerInput components
         rigidBody = GetComponent<Rigidbody>();
         input = GetComponent<PlayerInput>();
@@ -42,7 +125,8 @@ public class VehicleMovement : MonoBehaviour {
         drag = driveForce / terminalVelocity;
     }
 
-    void FixedUpdate() {
+
+    private void FixedUpdate() {
         //Calculate the current speed by using the dot product. This tells us
         //how much of the ship's velocity is in the "forward" direction 
         speed = Vector3.Dot(rigidBody.velocity, transform.forward);
@@ -54,7 +138,8 @@ public class VehicleMovement : MonoBehaviour {
         CalculatePropulsion();
     }
 
-    void CalculatHover() {
+
+    private void CalculatHover() {
         //This variable will hold the "normal" of the ground. Think of it as a line
         //the points "up" from the surface of the ground
         Vector3 groundNormal;
@@ -87,9 +172,7 @@ public class VehicleMovement : MonoBehaviour {
             //...and finally apply the hover and gravity forces
             rigidBody.AddForce(force, ForceMode.Acceleration);
             rigidBody.AddForce(gravity, ForceMode.Acceleration);
-        }
-        //...Otherwise...
-        else {
+        } else {
             //...use Up to represent the "ground normal". This will cause our ship to
             //self-right itself in a case where it flips over
             groundNormal = Vector3.up;
@@ -120,7 +203,8 @@ public class VehicleMovement : MonoBehaviour {
         shipBody.rotation = Quaternion.Lerp(shipBody.rotation, bodyRotation, Time.deltaTime * 10f);
     }
 
-    void CalculatePropulsion() {
+
+    private void CalculatePropulsion() {
         //Calculate the yaw torque based on the rudder and current angular velocity
         float rotationTorque = input.rudder - rigidBody.angularVelocity.y;
         //Apply the torque to the ship's Y axis
@@ -164,7 +248,8 @@ public class VehicleMovement : MonoBehaviour {
         wheelsBack.localRotation = Quaternion.Euler(-wheelAngle, 0, 0);
     }
 
-    void OnCollisionStay(Collision collision) {
+
+    private void OnCollisionStay(Collision collision) {
         if (collision.gameObject.layer == LayerMask.NameToLayer("Wall")) {
             //...calculate how much upward impulse is generated and then push the vehicle down by that amount 
             //to keep it stuck on the track (instead up popping up over the wall)
@@ -172,6 +257,7 @@ public class VehicleMovement : MonoBehaviour {
             rigidBody.AddForce(-upwardForceFromCollision, ForceMode.Impulse);
         }
     }
+
 
     public float GetSpeedPercentage() {
         //Returns the total percentage of speed the ship is traveling
