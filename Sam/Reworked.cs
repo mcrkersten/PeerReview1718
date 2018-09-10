5f2bd6a28d417cfc254227b186d86002bb6a4318
ManOfCheese <36536353+ManOfCheese@users.noreply.github.com>
2018-09-10 10:49:15 +0200

    ReworkedCode-v2
    Made changes based on feedback from Timo Noordzee.

--- Sam/Reworked.cs
+++ Sam/Reworked.cs
@@ -2,32 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AltarActivate : MonoBehaviour
-{
+public class AltarActivate : MonoBehaviour {
 
     private GameObject objectOnAltar;
     public GameObject Door { get; set; }
-    public PlayerItemSearch PlayerItemSearch { get; set; }
+    public PlayerItemSearch ItemSearch { get; set; }
     public bool DoorOn { get; set; }
     public string KeyTag { get; set; }
 
 
     private void OnTriggerEnter(Collider other) {                                        //when something enters the collider
         if (other.tag == KeyTag) {                                                       //and is has the tag keyTag
-            if (playerItemSearch.objectInHand != null) {                                 //and the players hand is not empty
-                playerItemSearch.DropItem();                                             //empty the players hand.
+            if (ItemSearch.objectInHand != null) {                                       //and the players hand is not empty
+                PlayerItemSearch.DropItem();                                             //empty the players hand.
                 ((Rigidbody)other.GetComponent(typeof(Rigidbody))).isKinematic = true;   //make the key kinematic
                 other.transform.position = transform.position + new Vector3(0, 2, 0);    //and set it's position perfectly above the altar
                 other.gameObject.layer = 0;                                              //place the object another layer to make it impossible to pick up.
-                if (DoorOn == true) { Door.SetActive(true); }                            //if the bool doorOn is true set the door gameobject to active.
-                else if (DoorOn == false) { Door.SetActive(false); }                     //otherwise if the doorOn bool is false set the door to inactive.
+                Door.SetActive(true);
             }
-            else if (playerItemSearch.objectInHand == null) {                            //if the players hand is empty
+            else if (ItemSearch.objectInHand == null) {                                  //if the players hand is empty
                 ((Rigidbody)other.GetComponent(typeof(Rigidbody))).isKinematic = true;   //do the exact same stuff except you don't empty their hand.
                 other.transform.position = transform.position + new Vector3(0, 2, 0);
                 other.gameObject.layer = 0;
-                if (DoorOn == true) { Door.SetActive(true); }
-                else if (DoorOn == false) { Door.SetActive(false); }
+                Door.SetActive(true);
             }
         }
     }
