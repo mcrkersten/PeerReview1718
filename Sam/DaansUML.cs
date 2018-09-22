5809afd1f4686132768a652c725d9e689f8ba94a
ManOfCheese <36536353+ManOfCheese@users.noreply.github.com>
2018-09-22 14:36:45 +0200

    Daan's UML gedaan
    An implementation of Daan's Class Diagram

---
+++ Sam/DaansUML.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour {
+
+    public Sprite shield; //sprite for shield pick-up
+    public Sprite slowMotion; //sprite for slow-motion pick-up
+
+    public void OnPickUp() {
+        //Pick ups item when the player collides with it.
+    }
+}
+
+public class PlayerController : MonoBehaviour {
+
+    public GameObject player; //The gameobject that is the player
+
+    //Shouldn't the be controls here maybe?
+}
+
+public class Enemy : MonoBehaviour {
+
+    public float Speed { get; set; } //speed at which the enemy moves
+    public GameObject Target { get; set; } //target the enemy moves towards
+    private float retreatDistance; //distane between target and enemy at which it retreats
+
+    public void PlayerDeath(Enemy enemy) {
+        //Kills the player
+    }
+
+}
+
+public class Car : Enemy {
+
+    public Color CarColor { get; set; } //Determines color of the car.
+
+}
+
+public class Crocodile : Enemy {
+
+    public float Size { get; set; } //Determines the size of the crocodile.
+
+}
+
+public class Snake : Enemy () {
+
+    public Color SnakeColor { get; set; } //color of snek
+    public GameObject Projectile { get; set; } //snek spit
+
+    private void ShootAtPlayer() {
+        //snek spits saliva super satisfyingly
+    }
+
+}
+
+
