087b8d5f7cae0e5fd0b19a6980a9cfb78e1f8696
NVriezen <36536400+NVriezen@users.noreply.github.com>
2018-09-22 14:03:05 +0200

    Can's mooie UML met (hopelijk) verbeteringen van Niels

---
+++ NielsVriezen/Huiswerk/Les3/Player.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public int score;
+	protected int itemSelected;
+
+	//interface
+	protected float HP;
+	protected Item[] loot;
+	public string name {
+		get;
+		set;
+	}
+	protected Sprite texture;
+
+	public void canCraft(Recipe recipe){
+		if (recipe.canCraft (loot)) {
+			//iets met itemSelected om recept te gebruiken?
+			//Daarvoor mis ik wel een functie
+			recipe.craftRecipe();
+		}
+	}
+
+	public void damage (int val){
+		HP -= val;
+		if (HP <= 0){
+			kill ();
+		}
+	}
+
+	private void kill(){
+		//Get the loot and drop it
+		getLoot ();
+	}
+
+	public Item[] getLoot() {
+		return loot;
+	}
+
+	public float getHP(){
+		return HP;
+	}
+
+	public void move(Vector2 dir){
+		//input checken en Rigidbody2d.AddForce of met transform.position
+	}
+}
