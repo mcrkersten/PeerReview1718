36731fe62aaf2a356cd4986d7da961da1061b24e
Wessel Mast <mastwessel@gmail.com>
2018-09-27 08:09:54 +0200

    HW3, Mr. Jeremy

---
+++ Wessel/HW3/Sergants.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sergants : MainEnemy {
+
+    public override GameObject Weapon() {
+        return weapon;
+    }
+}
