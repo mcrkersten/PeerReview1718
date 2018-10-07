b394df0fa1657ef66fcdc404c1b602d2a2bde6b7
Daan de Bruijn <ddebruijn9@gmail.com>
2018-10-07 14:26:53 +0200

    Week 5 commit
    Geef me devpoints.
    Ik jullie kunnen dit reviewen om aan je limiet te voldoen. Ik heb wat inheritance uitgeprobeerd. Feedback is welkom!

---
+++ DaandB/Week 5/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	public static Enemy instance = null;
+
+	public float minSpeed = 1;
+	public float maxSpeed = 5;
+
+	public float speed;
+
+	// Speed variable; Min and max speed (see car script)
+	// Target GameObject (player)
+	// public retreatDistance (Perhaps a collider could make this unnecessary)
+
+	// Use this for initialization
+	void Start () {
+		speed = Random.Range(minSpeed, maxSpeed);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
